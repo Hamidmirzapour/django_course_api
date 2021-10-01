12c5dbcf1453ad9511ab7f785634f871ac0c724a
Hamid Mirzapour <h.r.mirzapour@gmail.com>
2021-10-01 10:51:58 +0330

    Create Course Model and add it to Admin Panel

--- course_api/course/models.py
+++ course_api/course/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class Course(models.Model):
+    name = CharField(max_length=200)
+    language = CharField(max_length=100)
+    price = CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
